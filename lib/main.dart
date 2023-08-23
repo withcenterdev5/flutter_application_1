386a70235939a0b72699d9524e420490db90dd4d
withcenterdev5 <withcenter.dev5@gmail.com>
2023-08-23 15:26:08 +0800

    secondtest

--- lib/main.dart
+++ lib/main.dart
@@ -122,3 +122,17 @@ class Test extends StatelessWidget {
     return const Placeholder();
   }
 }
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
