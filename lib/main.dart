94ce3c8507e9e8feb4b266b82df4556c7ac882cf
withcenterdev5 <withcenter.dev5@gmail.com>
2023-08-24 08:44:50 +0800

    pull update

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'New widget',
-      home: Scaffold( 
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('New widget!!!', 
-            style: TextStyle( 
-              color: Color.fromARGB(255, 156, 223, 158),
-              fontSize: 50,
-              
-            )
-          )
-        ),
-        body: ListView( 
-          children: <Widget>[
-            const ListTile( 
-              leading: const Icon(Icons.abc),
-              title: const Text('ABC!')
-            ),
-            const ListTile( 
-              leading: const Icon(Icons.abc_sharp),
-              title: const Text('ABC Sharp!')
-            )
-          ],
-        )
-      ),
-      
+      home: Scaffold(
+          appBar: AppBar(
+              centerTitle: true,
+              title: const Text('New widget!!!',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 156, 223, 158),
+                    fontSize: 50,
+                  ))),
+          body: ListView(
+            children: const <Widget>[
+              ListTile(leading: Icon(Icons.abc), title: Text('ABC!')),
+              ListTile(
+                  leading: Icon(Icons.abc_sharp), title: Text('ABC Sharp!'))
+            ],
+          )),
     );
   }
 }
